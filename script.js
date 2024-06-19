b1a0da668da4ad0159ea9973fd2470d8f3e0c820
Dmytro Kaplunskyi <dkaplunsky@gmail.com>
2024-06-19 12:39:55 -0400

    add functionality jsut for two inputs

--- script.js
+++ script.js
@@ -1,63 +1,58 @@
+function addition(x, y) {
+  return x + y;
+}
+function subtraction(x, y) {
+  return x - y;
+}
+function multiplication(x, y) {
+  return x * y;
+}
+function division(x, y) {
+  return x / y;
+}
+
+let num1 = undefined;
+let operator = '';
+let num2 = undefined;
+
+function operate(x, operator, y) {
+  switch (operator) {
+    case '+':
+      return addition(x, y);
+    case '-':
+      return subtraction(x, y);
+    case '*':
+      return multiplication(x, y);
+    case '/':
+      return division(x, y);
+  }
+}
+
+const digits = document.querySelectorAll('.digit');
 const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('.btn');
-const allClearBtn = document.querySelector('.all-clear');
+const operators = document.querySelectorAll('.symbol');
+const equal = document.querySelector('.equal-btn');
 
-const operators = ['+', '-', '*', '/', '=', '.'];
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let displayValue = undefined;
 
-allClearBtn.addEventListener('click', () => {
-  display.textContent = '0';
+digits.forEach((digit) => {
+  digit.addEventListener('click', (e) => {
+    display.textContent += e.target.textContent;
+    displayValue = display.textContent;
+  });
+})
+
+operators.forEach((symbol) => {
+  symbol.addEventListener('click', (e) => {
+    num1 = parseInt(displayValue);
+    operator = e.target.textContent;
+    display.textContent = '';
+  });
 });
 
-
-let getInput = () => parseInt(display.textContent);
-
-buttons.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    let userInput = e.target.textContent;
-    if (operators.includes(userInput)) {
-      switch (userInput) {
-        case '+':
-          // addition();
-          console.log('addition func');
-          break;
-        case '-':
-          // subtraction();
-          console.log('subtraction func');
-          break;
-        case '*':
-          // multiplication();
-          console.log('multiply func');
-          break;
-        case '/':
-          // division();
-          console.log('division func');
-          break;
-        case '=':
-          // result();
-          console.log('equal func');
-          break;
-        case '.':
-          // subtraction();
-          if (!display.textContent.includes('.')) {
-            display.textContent += '.';
-          }
-          console.log('dot func');
-          break;
-
-        default:
-          break;
-      }
-    } else if (numbers.includes(userInput)) {
-      if (display.textContent !== "0") {
-        display.textContent += userInput;
-        console.log(userInput);
-      } else {
-        display.textContent = '';
-        display.textContent = userInput;
-        console.log(userInput);
-      }
-    }
-  }
-  )
+equal.addEventListener('click', () => {
+  num2 = parseInt(displayValue);
+  let result = operate(num1, operator, num2);
+  display.textContent = result;
+  console.log(result);
 })
